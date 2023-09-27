a36efbff5846c14017dae76ecb1af419bf50a007
puju3366 <86496398+puju3366@users.noreply.github.com>
2023-09-27 19:47:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ echo '<!DOCTYPE html>
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <style>
 		#solar-system {
-		   background: url("https://raw.githubusercontent.com/puju3366/Solar-System-CI/dev/images/solar-system-3.png")   center center;
+		   background: url("https://raw.githubusercontent.com/puju3366/Solar-System-CI/dev/images/solar-system-9.png")   center center;
            background-repeat: no-repeat;			  
 		   background-size: cover;
 		   content: "";
